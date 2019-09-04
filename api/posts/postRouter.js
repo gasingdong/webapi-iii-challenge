11a5ac95e0e7e21aadb3039f6aafe99c2917f8bd
Brandon Dong <gasingdong@gmail.com>
2019-09-04 15:32:11 -0700

    implement get posts endpoint

--- api/posts/postRouter.js
+++ api/posts/postRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const postDb = require('./postDb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  (async () => {
+    try {
+      const posts = await postDb.get();
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(500).json({ error: 'The server could not retrieve posts.' });
+    }
+  })();
+});
 
 router.get('/:id', (req, res) => {});
 
