26e13296e4a9e7e415f27ddcada334b19cf9057c
Brandon Dong <gasingdong@gmail.com>
2019-09-04 13:29:02 -0700

    add logging

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 const logger = (req, res, next) => {
   console.log(
     `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
-      'Origin'
+      'host'
     )}`
   );
   next();
